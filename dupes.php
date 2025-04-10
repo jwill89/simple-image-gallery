80b89bfe574c00d688ef0906ce727ab6ac9ff2f2
James Will <jwillfiji@gmail.com>
2025-04-10 00:14:44 -0500

    Update duplicate job to use new stored bit fingerprint to create hash for comparison, significantly speeding up the script.

--- dupes.php
+++ dupes.php
@@ -7,9 +7,11 @@ ini_set('memory_limit','1024M');
 // Autoloader
 require('vendor/autoload.php');
 
+use Jenssegers\ImageHash\Hash;
 use Jenssegers\ImageHash\ImageHash;
 use Jenssegers\ImageHash\Implementations\DifferenceHash;
 use Gallery\Collection\ImageCollection;
+use Gallery\Structure\Image;
 
 // Set Time Limit for Script, 10 minutes
 set_time_limit(0);
@@ -27,44 +29,30 @@ $hasher = new ImageHash(new DifferenceHash());
 $matches = [];
 
 // Get the images already in the database
-$collection = new ImageCollection();
-$images_in_database = $collection->getAll();
+$image_collection = new ImageCollection();
+$images_in_database = $image_collection->getAll();
 
-// Get the images in the folder
-$images_in_folder = array_filter(scandir(ImageCollection::IMAGE_DIRECTORY_FULL), function($item) {
-	return !is_dir(ImageCollection::IMAGE_DIRECTORY_FULL . $item);
-});
-
-// Create folder hashes
-$folder_hashes = [];
-
-foreach ($images_in_folder as $key => $folder_file_name) {
-	
-	try {
-	
-		$folder_hash = $hasher->hash(ImageCollection::IMAGE_DIRECTORY_FULL . $folder_file_name);
-		
-		$folder_hashes[$key] = $folder_hash;
-	
-	} catch (Exception $e) {
-    
-		// ignore Error, continue
-	
-	}
-	
-}
-
+// Loop Through the Images in the Database
+/** @var Image $img */
 foreach ($images_in_database as $img) {
 	
 	try {
-	
-		$database_hash = $hasher->hash(ImageCollection::IMAGE_DIRECTORY_FULL . $img->getFileName());
 		
-		foreach ($folder_hashes as $key => $folder_hash) {
+		// Compare to every other image in the database
+		/** @var Image $img2 */
+		foreach ($images_in_database as $img2) {
+
+			// Skip if the images are the same
+			if ($img->getImageId() === $img2->getImageId()) {
+				continue;
+			}
+
+			// Convert fingerprints back to has items
+			$hash1 = Hash::fromBits($img->getBitsFingerprint());
+			$hash2 = Hash::fromBits($img2->getBitsFingerprint());
 					
-			if ($hasher->distance($database_hash, $folder_hash) <= 2) {
-				
-				$matches[] = [$img->getFileName(), $images_in_folder[$key]];
+			if ($hasher->distance($hash1, $hash2) <= 2) {
+				$matches[] = [$img->getImageId(), $img2->getImageId()];
 				
 			}
 			
@@ -73,35 +61,18 @@ foreach ($images_in_database as $img) {
 	} catch (Exception $e) {
     
 		// ignore Error, continue
+		continue;
 	
-	}
-	
-}
-
-// We only want file names that aren't the same
-$differences = [];
-
-foreach ($matches as $item) {
-	
-	if ($item[0] !== $item[1]) {
-		
-		// Add to differences if it's opposite isn't already in the array
-		if (!in_array([$item[1], $item[0]], $differences)) {
-		
-			$differences[] = $item;
-			
-		}
-		
 	}
 	
 }
 
 // Save to File if we have matches
-if (!empty($differences)) {
+if (!empty($matches)) {
 	
 	$json_file = fopen('dupes/dupes-' . date('Y-m-d') . '.json', 'w');
 	
-	fwrite($json_file, json_encode($differences));
+	fwrite($json_file, json_encode($matches));
 	
 	fclose($json_file);
 	
